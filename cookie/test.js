3111e249b2e99c5e9dcac170ee2ac77b6f5d3172
luojiyin <luojiyin@hotmail.com>
2022-07-05 15:18:05 +0800

    error: after need ";"

---
+++ cookie/test.js
@@ -0,0 +1,23 @@
+const app = require('./app')
+const server = app.listen()
+const request = require('supertest').agent(server)
+
+describe('Cookies Views',()=> {
+    after(()=> {
+        server.close()
+      })
+
+
+    [1, 2, 3].forEach( (i)=> {
+        describe('on iteration #' + i, ()=> {
+            it('should set the views as a cookies and as the body', (done) => {
+                request
+                    .get('/')
+                    .expect(200)
+                    .expect('Set-Cookie', new RegExp('view=' + i))
+                    .expect(i + ' views', done)
+            })
+        })
+    });
+})
+
